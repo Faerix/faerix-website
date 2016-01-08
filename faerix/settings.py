a5ba794a541300e8f7645917ef56d00c0da4c56f
Symphorien Gibol <symphorien.gibol@gmail.com>
2016-01-08 11:19:20 +0100

    set upload root to /media

--- faerix/settings.py
+++ faerix/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MEDIA_URL = '/media/'
 

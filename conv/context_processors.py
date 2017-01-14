87165e99a50c6389dbec9437a742b3e1e2441b6c
Swynfel <swynfel@gmail.com>
2017-01-14 13:54:12 +0100

    Make old sponsors still displayable in old versions

--- conv/context_processors.py
+++ conv/context_processors.py
@@ -4,16 +4,24 @@ from django.conf import settings
 import random
 
 def inject_sponsors(request):
-    print(list(request.GET))
-    year=request.GET.get("year",currentConv())
-    sponsors = list(Sponsor.objects.filter(active=True,conv=year))
-    random.shuffle(sponsors)
-    return {"sponsor_logos":sponsors}
+	path=getattr(request,"path","")
+	year=currentConv()
+	try :
+	#Extremly dirty but I can't find why the request.GET is always empty
+	#otherwise request.GET.get("year",currentConv()) would have worked
+		if (path[:5])=="/old/":
+			year = int(path[5:9])
+	finally:
+		True
+	sponsors = list(Sponsor.objects.filter(active=True,conv=year))
+	random.shuffle(sponsors)
+	return {"sponsor_logos":sponsors,"year":1111111}
 
 def inject_news_count(request):
-    count = News.objects.filter(visible_from__lte=timezone.now(), visible_up_to__gte=timezone.now()).count()
-    return {"news_count" :count}
+	count = News.objects.filter(visible_from__lte=timezone.now(), visible_up_to__gte=timezone.now()).count()
+	return {"news_count" :count}
 
 def inject_static_url(request):
-    # for django-front
-    return {"STATIC_URL": settings.STATIC_URL}
+	# for django-front
+	print(list(request))
+	return {"STATIC_URL": settings.STATIC_URL}
--- faerix/settings.py
+++ faerix/settings.py
@@ -74,18 +74,28 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'conv.context_processors.inject_sponsors',
-                'conv.context_processors.inject_news_count',
-                'conv.context_processors.inject_static_url',
-
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'conv.context_processors.inject_sponsors',
+                'conv.context_processors.inject_news_count',
+                'conv.context_processors.inject_static_url',
             ],
         },
     },
 ]
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+
+)
+
 
 WSGI_APPLICATION = 'faerix.wsgi.application'
 

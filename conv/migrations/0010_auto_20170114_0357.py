0b4408157f2d746df52cc318f9148afa7541bc53
Symphorien Gibol <symphorien+git@xlumurb.eu>
2017-03-17 19:31:43 +0100

    fix migrations ?

--- conv/migrations/0010_auto_20170114_0357.py
+++ conv/migrations/0010_auto_20170114_0357.py
@@ -11,29 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Edition',
-            fields=[
-                ('year', models.PositiveIntegerField(serialize=False, verbose_name='Année de la Conv', primary_key=True)),
-                ('start', models.DateField(verbose_name='Jour de début de la Conv')),
-                ('end', models.DateField(verbose_name='Jour de fin de la Conv')),
-            ],
-        ),
-        migrations.AddField(
-            model_name='event',
-            name='conv',
-            field=models.ForeignKey(to='conv.Edition', default=2017, related_name='even_conv'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='scenario',
-            name='conv',
-            field=models.ForeignKey(to='conv.Edition', default=2017, related_name='conv'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='user',
-            name='editions',
-            field=models.ManyToManyField(db_constraint='Editions this user attendented', to='conv.Edition'),
-        ),
     ]
